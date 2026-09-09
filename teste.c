dfa93a744ae8f9e537f7b46b682c7c661772f40f
PriettoDev <284988517+PriettoDev@users.noreply.github.com>
2026-09-09 11:28:18 -0300

    Style: Mudança de organização no arquivo functions.txt

---
+++ teste.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(){
+//    printf("Bem-vindos ao /mundo\\ da programaçao \"C\"");
+// //    puts("Hello World"); 
+//     int num = 123;
+//     printf("O valor de num= %d e o valor seguinte = %d\n",num,num+1);
+//     printf("O tamanho em bytes de um inteiro eh: %d", sizeof(double));
+// ----------------------------------
+// char ch1, ch2;
+    // printf("Introduza um caractere: ");
+    // scanf("%c", &ch1);
+    // printf("Introduza outro caractere: ");
+    // scanf(" %c", &ch2);
+    // printf("Os caracteres inseridos foram '%c', e '%c'\n", ch1,ch2);
+    // ----------------------------
+    // int num;
+    // printf("Escreva um numero: ");
+    // scanf("%d", &num);
+    // printf("O numero digitado eh: %d, cujo caractere correspondente eh o: %c\n", num, (char) num);
+    // printf("O proximo numero seria o: %d, cujo caractere correspondente eh o: %c\n", num+1, (char) num+1);
+    // ------------------------
+    // int dd, mm, aa;
+    // printf("Digite uma data: ");
+    // scanf("%d%d%d", &dd, &mm, &aa);
+    // printf("%d/%d/%d", dd, mm, aa);
+    // ---------------------
+    // float salario;
+    // printf("Digite o seu salario: ");
+    // scanf("%f", &salario);
+    // if(salario<=0){
+    //     printf("Erro!");
+    // }
+    // else if(salario<1000 && salario>0){
+    //     salario = salario-(salario*0.05);
+    //     printf("Seu salario com os impostos eh: %.2f", salario);
+    // }
+    // else{
+    //     salario = salario-(salario*0.15);
+    //     printf("Seu salario com os impostos eh: %.2f", salario);
+    // }
+    
+}
